0715db01885f2b45d23f288ede20f07965ce994d
adammartin <ebondark.od@gmail.com>
2015-03-10 08:11:33 -0500

    Dealing with evil of Microsoft

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,6 +1,25 @@
 require 'main'
 require 'csv'
 
+
+module OS
+  def OS.windows?
+    (/cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM) != nil
+  end
+
+  def OS.mac?
+    (/darwin/ =~ RUBY_PLATFORM) != nil
+  end
+
+  def OS.unix?
+    !OS.windows?
+  end
+
+  def OS.linux?
+    OS.unix? and not OS.mac?
+  end
+end
+
 Main {
   argument('source_file_path') {
     description "The full path of the source csv file"
@@ -10,7 +29,9 @@ Main {
   }
   def run
     multimap = Hash.new{ | h, k| h[k] = [] }
-    CSV.foreach(params['source_file_path'].value) do | row |
+    options = {}
+    options[:encoding] = 'windows-1251:utf-8' if OS.windows?
+    CSV.foreach(params['source_file_path'].value, options) do | row |
       (multimap[row.shift] <<  row.join(',')) if (/^(?<num>\d+)$/ =~ row[0])
     end
 
